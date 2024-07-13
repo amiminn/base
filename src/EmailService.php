cc8925cae3af7eaa1d5353fda9e18e98390bbc89
Tholabul Amin <104202968+amiminn@users.noreply.github.com>
2024-07-13 09:27:35 +0700

    Update EmailService

--- src/EmailService.php
+++ src/EmailService.php
@@ -6,17 +6,17 @@ use Illuminate\Support\Facades\Mail;
 
 class EmailService
 {
-    public static function send($email_tujuan, $subject, $path, $data = [], $files = [])
+    public static function send($email_tujuan, $subject, $path, $from="Yo Amiminn Official", $data = [], $files = [])
     {
         try {
             $maindata = [
                 'data' => $data
             ];
 
-            Mail::send('mailTemplates.' . $path, $maindata, function ($message) use ($email_tujuan, $subject, $files) {
+            Mail::send('mailTemplates.' . $path, $maindata, function ($message) use ($email_tujuan, $subject, $files, $from) {
                 $message->to($email_tujuan)
                     ->subject($subject);
-
+                $message->from("admin@amiminn.my.id", $from);
                 foreach ($files as $file) {
                     $message->attach($file);
                 }
